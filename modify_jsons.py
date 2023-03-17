f65d8f7a4db26299ab89f982549604a657e16258
anastzel <tzelepakis.a@gmail.com>
2023-03-17 11:56:12 +0200

    Modified modify_jsons and added comments

--- modify_jsons.py
+++ modify_jsons.py
@@ -2,22 +2,34 @@ import os
 import json
 from tqdm import tqdm
 
+# This was created to fix the imagePath attrubute mistake in the generated json
+# file after augmentation. It might not be useful anymore.
+
+# Define the source directory containing the json annotations
 src_dir = "dataset"
 
-input_dirs = [os.path.join(src_dir, "train"), os.path.join(src_dir, "val"), os.path.join(src_dir, "test")]
+input_train_dir = os.path.join(src_dir, "train")
+input_val_dir = os.path.join(src_dir, "val")
+input_test_dir = os.path.join(src_dir, "test")
 
-for input_dir in input_dirs:
-    filenames = os.listdir(input_dir)
+# Define the directories to run through
+input_dirs = [input_train_dir, input_val_dir, input_test_dir]
+
+for dir in input_dirs:
+    # Get the list of files in the directory
+    filenames = os.listdir(dir)
     for filename in tqdm(filenames, total=len(filenames)):
+        # If file is a json file
         if filename.endswith('json'):
             
             # Open the JSON file
-            with open(os.path.join(input_dir, filename), 'r') as file:
+            with open(os.path.join(dir, filename), 'r') as file:
+                # Read the JSON file
                 data = json.load(file)
 
             # Update the ImagePath attribute
             data["imagePath"] = filename.replace('json', 'jpg')
 
             # Save the updated JSON file
-            with open(os.path.join(input_dir, filename), 'w') as file:
+            with open(os.path.join(dir, filename), 'w') as file:
                 json.dump(data, file)
