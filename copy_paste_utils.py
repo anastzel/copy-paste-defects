db29790d3792936a0ae850c43b4978dd3f8f2fd7
anastzel <tzelepakis.a@gmail.com>
2023-03-17 12:42:58 +0200

    Added general and copy_paste utility functions

--- copy_paste_utils.py
+++ copy_paste_utils.py
@@ -4,3 +4,21 @@ Created on Fri Mar 17 12:02:00 2023
 
 @author: anastzel
 """
+
+import os
+
+def get_images_with_specific_defect(dir, defect_type):
+    
+    defect_dir = os.path.join(dir, defect_type)
+    defect_images_paths = [os.path.join(defect_dir, filename) for filename in os.listdir(defect_dir) if filename.endswith('.jpg')]
+
+    return defect_images_paths
+
+
+if __name__ == '__main__':
+    
+    # For debugging purposes
+    source_dir = r"D:\copy_paste_pipeline\source_images"
+    defect_type = "hole"
+    for path in get_images_with_specific_defect(source_dir, defect_type):
+        print(path)
--- general_utils.py
+++ general_utils.py
@@ -4,3 +4,25 @@ Created on Fri Mar 17 12:19:00 2023
 
 @author: anastzel
 """
+
+import random
+
+def get_random_list_item(input_list):
+    """
+    Returns a random iteam from a list.
+    """
+
+    return random.choice(input_list)
+
+def get_random_list_id(input_list):
+    """
+    Returns a random index from a list.
+    """
+    return random.randint(0, len(input_list) - 1)
+
+if __name__ == "__main__":
+
+    # For debugging
+    input_list = [1, 2, 3, 4, 5]
+    print(get_random_list_id(input_list))
+    print(get_random_list_item(input_list))
